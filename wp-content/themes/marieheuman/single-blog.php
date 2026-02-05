64e7d298e7f648ab9d2dccd66d630ed9ef4355e8
Lucas <lucas.detling@gmail.com>
2026-02-05 10:47:05 +0100

    blog aside fixe content

--- wp-content/themes/marieheuman/single-blog.php
+++ wp-content/themes/marieheuman/single-blog.php
@@ -10,21 +10,29 @@ get_header();
             $content = get_field('contenu_page_blog');
             ?>
             <h1><?= the_title() ?></h1>
-            <div class="description">
-                <?= $content['description'] ?>
-            </div>
-            <p class="blog-meta">
-                <span class="date">
-                    <?= get_the_date('F o'); ?> •
-                    <?= $content['temps_de_lecture'] ?> de lecture
-                </span>
-            </p>
-            <img src="<?= $content['image']['url'] ?>" alt="<?= $content['image']['alt'] ?>">
+            <?php if (!empty($content['description'])): ?>
+                <div class="description">
+                    <?= $content['description'] ?>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($content['temps_de_lecture'])): ?>
+                <p class="blog-meta">
+                    <span class="date">
+                        <?= get_the_date('F o'); ?> •
+                        <?= $content['temps_de_lecture'] ?> de lecture
+                    </span>
+                </p>
+            <?php endif; ?>
+            <?php if (!empty($content['image'])): ?>
+                <img src="<?= $content['image']['url'] ?>" alt="<?= $content['image']['alt'] ?>">
+            <?php endif; ?>
         </div>
         <!-- END HEADER -->
-        <div class="content">
-            <?= $content['contenu'] ?>
-        </div>
+        <?php if (!empty($content['contenu'])): ?>
+            <div class="content">
+                <?= $content['contenu'] ?>
+            </div>
+        <?php endif; ?>
     </section>
     <aside class="sidebar lg:w-2/8 section-beige">
         <?php
@@ -47,57 +55,58 @@ get_header();
             </div>
         </form>
         <!-- INFORMATIONS -->
-        <?php if (!empty($sidebar['informations'])): ?>
-            <div class="informations">
-                <h3>
-                    <?= $sidebar['informations']['titre'] ?>
-                </h3>
-                <img src="<?= $sidebar['informations']['image']['url'] ?>"
-                    alt="<?= $sidebar['informations']['image']['alt'] ?>">
-                <?= $sidebar['informations']['description'] ?>
-            </div>
-        <?php endif; ?>
+        <div class="informations">
+            <h3>
+                Bonjour, Je suis Marie Heuman
+            </h3>
+            <img src="<?= get_template_directory_uri() ?>/assets/images/Marie-Heuman.webp" alt="Photo de Marie Heuman">
+            <p>Architecte d’intérieur & designer global depuis 2021.</p>
+            <p>À travers ce blog, Histoires d’intérieurs, je partage mes conseils, inspirations et réflexions autour de
+                l’architecture intérieure et du design.</p>
+            <p>C’est aussi un espace plus personnel, où je vous ouvre les coulisses de mon métier, mes découvertes, mes
+                doutes parfois, et surtout la passion qui m’anime.</p>
+            <p>Ici, on parle autant de technique que de sensibilité, de matières que de vécu, parce qu’un intérieur bien
+                pensé, c’est avant tout une rencontre entre esthétique et humanité.</p>
+        </div>
 
         <!-- END INFORMATIONS -->
 
         <!-- HISTOIRE -->
-        <?php if (!empty($sidebar['histoire'])): ?>
-            <div class="histoire">
-                <h3>
-                    <?= $sidebar['histoire']['titre'] ?>
-                </h3>
-                <a href="<?= $sidebar['histoire']['lien']['url'] ?>"
-                    class="more"><?= $sidebar['histoire']['lien']['title'] ?></a>
-            </div>
-        <?php endif; ?>
+        <div class="histoire">
+            <h3>
+                Envie d’en savoir plus sur mon parcours ?
+            </h3>
+            <a href="mon-histoire-architecte-interieur-tours-blois" class="more">Découvrez mon histoire</a>
+        </div>
 
         <!-- END HISTOIRE -->
 
         <!-- ACCOMPAGNEMENTS -->
-        <?php if (!empty($sidebar['accompagnements'])): ?>
-            <div class="accompagnements">
-                <h3>
-                    <?= $sidebar['accompagnements']['titre'] ?>
-                </h3>
-                <?= $sidebar['accompagnements']['description'] ?>
-                <a href="<?= $sidebar['accompagnements']['lien']['url'] ?>"
-                    class="more"><?= $sidebar['accompagnements']['lien']['title'] ?></a>
-            </div>
-        <?php endif; ?>
+        <div class="accompagnements">
+            <h3>
+                Découvrez mes accompagnements
+            </h3>
+            <p>Vous avez un projet d’aménagement, de rénovation ou de création d’identité ?</p>
+            <p>Mes accompagnements sont pensés pour s’adapter à votre rythme, vos besoins et votre univers.</p>
+            <p>De la première idée à la concrétisation, je vous guide à chaque étape.</p>
+            <a href="expertise-accompagnement-architecture-interieure-tours-blois" class="more">Découvrez mes
+                services</a>
+        </div>
 
         <!-- END ACCOMPAGNEMENTS -->
 
         <!-- RESERVATION -->
-        <?php if (!empty($sidebar['reservation'])): ?>
-            <div class="reservation">
-                <h3>
-                    <?= $sidebar['reservation']['titre'] ?>
-                </h3>
-                <?= $sidebar['reservation']['description'] ?>
-                <a href="<?= $sidebar['reservation']['lien']['url'] ?>"
-                    class="more"><?= $sidebar['reservation']['lien']['title'] ?></a>
-            </div>
-        <?php endif; ?>
+        <div class="reservation">
+            <h3>
+                Réservez votre appel découverte offert
+            </h3>
+            <p>Vous avez un projet en tête, une question ou simplement envie d’échanger ?</p>
+            <p>Prenons le temps d’en parler ensemble.</p>
+            <p>Cet appel gratuit et sans engagement me permet de comprendre vos besoins, de vous guider sur la bonne
+                formule et de poser les premières bases d’un projet réfléchi et serein.</p>
+            <a href="contact-architecte-interieur-tours-blois/#custom-booking-app" class="more">Réservez votre
+                créneau</a>
+        </div>
 
         <!-- END RESERVATION -->
 

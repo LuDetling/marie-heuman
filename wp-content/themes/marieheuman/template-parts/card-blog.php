1da7e7660e1ec2857de2f9df6454d0d9825130bf
Lucas <lucas.detling@gmail.com>
2026-02-24 15:38:41 +0100

    content blog

--- wp-content/themes/marieheuman/template-parts/card-blog.php
+++ wp-content/themes/marieheuman/template-parts/card-blog.php
@@ -3,17 +3,20 @@ $blog = get_field('contenu_page_blog');
 ?>
 <div class="card-article section-white">
     <?php if ($blog['image']): ?>
-        <img src="<?= esc_url($blog['image']['url']); ?>" alt="<?= esc_attr($blog['image']['alt']); ?>"
-            width="<?= esc_attr($blog['image']['width']); ?>" height="<?= esc_attr($blog['image']['height']); ?>">
+        <div class="img-link">
+            <img src="<?= esc_url($blog['image']['url']); ?>" alt="<?= esc_attr($blog['image']['alt']); ?>"
+                width="<?= esc_attr($blog['image']['width']); ?>" height="<?= esc_attr($blog['image']['height']); ?>">
+            <a href="<?= the_permalink(); ?>"></a>
+        </div>
     <?php endif; ?>
     <div class="text-card">
         <h4><?php the_title(); ?></h4>
         <p class="blog-meta">
             <span class="date">
-                <?php echo get_the_date('F o'); ?> •
+                <?= get_the_date('F o'); ?> •
             </span>
             <?= $blog['temps_de_lecture'] ?> de lecture
         </p>
-        <a href="<?php the_permalink(); ?>" class="more mt-4">Lire l'article</a>
+        <a href="<?= the_permalink(); ?>" class="more mt-4">Lire l'article</a>
     </div>
 </div>
